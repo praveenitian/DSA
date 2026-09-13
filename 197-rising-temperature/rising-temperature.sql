a8f649ddf00879b7158f0445a6e8a34af984790e
praveenitian <162729605+praveenitian@users.noreply.github.com>
2026-09-13 10:55:12 +0530

    Time: 427 ms (87.53%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -5,11 +5,24 @@
 -- on datediff(a.recordDate,b.recordDate)=1
 -- where a.temperature>b.temperature;
 
-select id
-from (
+
+-- SUBQUERY METHOD
+-- select id
+-- from (
+--     select id,recordDate,temperature,
+--     lag(temperature) over(order by recordDate) as prev_temp,
+--     lag(recordDate) over(order by recordDate) as prev_date
+--     from Weather
+-- ) as temphistory
+-- where temperature>prev_temp and datediff(recordDate,prev_date)=1
+
+with weatherhistory as (
     select id,recordDate,temperature,
     lag(temperature) over(order by recordDate) as prev_temp,
     lag(recordDate) over(order by recordDate) as prev_date
     from Weather
-) as temphistory
-where temperature>prev_temp and datediff(recordDate,prev_date)=1
+)
+
+select id
+from weatherhistory
+where temperature>prev_temp and datediff(recordDate,prev_date)=1;
